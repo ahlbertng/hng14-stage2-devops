11d6a215bb09ddfe62813c902e2b0834be66f46c
Ahlbertng <70105688+ahlbertng@users.noreply.github.com>
2026-04-23 20:09:30 +0100

    Remove unnecessary newlines for improved code clarity in main.py

--- api/main.py
+++ api/main.py
@@ -10,7 +10,6 @@ REDIS_PORT = int(os.getenv("REDIS_PORT"))
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 
-
 @app.post("/jobs")
 def create_job():
     job_id = str(uuid.uuid4())
@@ -19,14 +18,12 @@ def create_job():
     return {"job_id": job_id}
 
 
-
 @app.get("/jobs/{job_id}")
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
         return {"error": "not found"}
     return {"job_id": job_id, "status": status.decode()}
-
 
 
 @app.get("/health")
